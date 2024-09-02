da6889816ac7286df38b38677867dfbfdc7aba00
Robert Schütz <mail@dotlambda.de>
2024-09-02 14:05:00 -0700

    python312Packages.bimmer-connected: 0.16.2 -> 0.16.3
    Diff: https://github.com/bimmerconnected/bimmer_connected/compare/refs/tags/0.16.2...0.16.3
    
    Changelog: https://github.com/bimmerconnected/bimmer_connected/releases/tag/0.16.3

--- pkgs/development/python-modules/bimmer-connected/default.nix
+++ pkgs/development/python-modules/bimmer-connected/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "bimmer-connected";
-  version = "0.16.2";
+  version = "0.16.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "bimmerconnected";
     repo = "bimmer_connected";
     rev = "refs/tags/${version}";
-    hash = "sha256-9v1GDkpw9gCfjBX4t9ApQkyo4s6gXf4lo/fkJG1QNbs=";
+    hash = "sha256-IrGOhUnWTtCI5juFFuNdWSWxeFr7s8bRNT8sUludGo0=";
   };
 
   build-system = [
